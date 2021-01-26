354e264f7201c9a48c1f181f1a200f12ed98e030
irvinglink <irving.link@gmail.com>
2021-01-25 20:08:00 -0700

    Ping Test

--- index.js
+++ index.js
@@ -23,12 +23,18 @@ client.on("ready", () => {
 
 
   util.status('mcwonderland.net', { port: 25556, enableSRV: true, timeout: 5000, protocolVersion: 47 })
+
   .then((response) => {
-    isServerOnline = true;
+
+    var isServerOnline = true;
+
   })
+
     .catch((error) => {
-        isServerOnline = false;
+
+        var isServerOnline = false;
         throw error;
+
     });
 
 
@@ -69,9 +75,7 @@ client.on('message', async msg => {
 
         case "ip":
 
-            if (isServerOnline) msg.channel.send(`IP del servidor: \`mcwonderland.net\` \nVersion 1.16.4
-
-            \n Jugadores conectados ${status_response.onlinePlayers} / ${status_response.maxPlayers}`);
+            if (isServerOnline) msg.channel.send(`IP del servidor: \`mcwonderland.net\` \nVersion 1.16.4 \nJugadores conectados ${status_response.onlinePlayers} / ${status_response.maxPlayers}`);
 
             else msg.channel.send(`IP del servidor: \`mcwonderland.net\` \nVersion 1.16.4 \n**El servidor se encuentra apagado**`);
 
