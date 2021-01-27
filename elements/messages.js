20c0fe6b935cc9ccf19899f3a61b0e25d7ca322a
irvinglink <irving.link@gmail.com>
2021-01-26 22:27:02 -0700

    Commit

--- elements/messages.js
+++ elements/messages.js
@@ -1,13 +1,12 @@
 const discord = require("discord.js");
 
 export function shop_message() {
-    const shop_Message = new discord.MessageEmbed()
+
+    return new discord.MessageEmbed()
     .setColor("#6DD5FA")
     .setTitle("MCWonderLand")
     .setAuthor("MCWonderLand")
     .setDescription("Nosotros contamos con una tienda donde puedes adquirir rangos, o beneficios dentro del servidor.")
-    .addField("Pagina", "https://mcwonderland.tebex.io");
-
-    return shop_Message;
+    .addField("Pagina", "https://mcwonderland.tebex.io");;
 
 }
